33ca41e6d0af2a48712bbcb344d9d923ca5c4aca
Louyu000 <louyu50310@gmail.com>
2023-01-03 18:10:20 +0800

    UPDATE API

--- pages/Youtube.swift
+++ pages/Youtube.swift
@@ -12,8 +12,10 @@ struct SearchResponse:Codable{
 }
 struct Constants {
     static let youtubeAPI_KEY = "AIzaSyCEZqy9n5BfHTK72Bl_zqy1ILkF0dq64iw"
+    static let youtubeChannelURL = "https://www.googleapis.com/youtube/v3/channels?part=contentDetails&id="
     static let youtubePlaylistURL = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId="
-    static let channellistid = "UUtG2ospXrllukLp5TorrKzw"
+    static let channelid = "UCtG2ospXrllukLp5TorrKzw"
+    static var channellistid = "UUtG2ospXrllukLp5TorrKzw"
 }
 struct videopack:Codable,Identifiable{
     var id : String { snippet.title }
@@ -37,3 +39,16 @@ struct idinfo:Codable{
     let videoId :String
 }
 
+
+struct ChannelResponse:Codable{
+    let items:[channelinfo]
+}
+struct channelinfo:Codable{
+    let contentDetails:contentinfo
+}
+struct contentinfo:Codable{
+    let relatedPlaylists:channelid
+}
+struct channelid:Codable{
+    let uploads:String
+}
